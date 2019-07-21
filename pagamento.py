a033a9a4275ddfa5eeaaa703c088b160c436e060
Rodrigo Ramos <stuart-alt@live.com>
2019-07-21 13:11:58 -0300

    Delete pagamento.py

--- pagamento.py
+++
@@ -1,9 +0,0 @@
-print('{:-^40}'.format(' Calculadora '))
-total = float(input('Total da conta: '))
-pes = int(input('Quantas pessoas: '))
-
-total = total / pes
-
-print(f'Cada pessoa irá pagar: R$ {total:.2f}')
-semgarcom = total - (total * 0.10)
-print(f'Sem a gorjeta: R$ {semgarcom:.2f}')

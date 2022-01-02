4562ff1048b55fa7e254a871a5ea35fb7c5393b5
Rodrigo Guimarães <stuart-alt@live.com>
2022-01-02 11:04:37 -0300

    Melhorando a megasena

--- megasena.py
+++ megasena.py
@@ -1,38 +1,17 @@
 import random
 
-n1 = int(input("N1: "))
-n2 = int(input("N2: "))
+escolhidos = list()
+sorteados = list()
+for n in range(6):
+    escolhidos.append(int(input(f"N{n+1}: ")))
+    sorteados.append(random.randint(1, 60))
 
-lista = [n1, n2]
-lista.sort()
+escolhidos.sort()
+sorteados.sort()
 
-print(lista)
+print(escolhidos, '\n', sorteados)
 
-
-
-
-'''
-num = [0]
-
-for i in num:
-    num[i] = int(input('Digite o numero: '))
-    num[i] = int(input('Digite o numero: '))
-    num[i] = int(input('Digite o numero: '))
-    print(num[i])
-    i += 1
-
-
-#numeros = sort1 = random.randint(1,5)
-#numeros = sort2 = random.randint(1,5)
-#numeros = [1,5,4,3,2]
-num.sort()
-
-print(num)
-#print(sort1)
-#print(sort2)
-
-#if sort1 == num1 & sort2 == num2:
-#    print ("Acertou!")
-#else:
-#    print("Ma sorte")
-'''
+if escolhidos == sorteados:
+    print("Acertou!")
+else:
+    print("Ma sorte")
